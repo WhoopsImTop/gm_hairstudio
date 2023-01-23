a6f827749a74d11f1049a020ac3d93dd5798977d
Elias4711 <elias.englen@geops.de>
2023-01-23 18:33:54 +0100

    add new

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  generate: {},
+  generate: {
+    dir: "public",
+  },
 
   router: {
     scrollBehavior(to) {
