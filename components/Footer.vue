bff87856789e340f5a3bd9e9fded1d6cdc200807
Elias4711 <elias.englen@geops.de>
2023-01-29 14:36:11 +0100

    add enw

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="footer-container">
     <div class="content-container">
+      <div class="row lined">
+        <div class="col">
+          <h4 style="text-transform: uppercase; margin-bottom: 0px;">Öffnungszeiten</h4>
+          <p style="margin-top: 10px">Mo-Sa: 9:00 - 17:00 Uhr</p>
+        </div>
+        <div class="col">
+          <h4 style="text-transform: uppercase; margin-bottom: 0px;">Kontakt</h4>
+          <p style="margin: 10px 0 0 0;"><a style="text-decoration: none" href="mailto:info@gmhairstudio.de">info@gmhairstudio.de</a></p>
+          <p style="margin-top: 5px"><a style="text-decoration: none" href="tel:0761 51463046">0761 51463046</a></p>
+        </div>
+        <div class="col">
+          <h4 style="text-transform: uppercase; margin-bottom: 0px;">Adresse</h4>
+          <p style="margin: 10px 0 0 0;">Ellen-Gottlieb-Straße 15</p>
+          <p style="margin-top: 5px">79106 Freiburg im Breisgau</p>
+        </div>
+      </div>
       <div class="row">
         <div class="row">
           <a class="credencials" href="https://eliasenglen-grafikdesign.de" target="_blank"
@@ -54,6 +70,12 @@ a.credencials {
   margin-right: 30px;
 }
 
+.row.lined {
+  margin-bottom: 20px;
+  border-bottom: 1px solid #efefef;
+  padding-bottom: 20px;
+  align-items: flex-start;
+}
 @media (max-width: 900px) {
   .row {
     flex-direction: column;
